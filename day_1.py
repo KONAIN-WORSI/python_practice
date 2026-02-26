69af03e568a14826123d16cd40a868170c97a1f0
KONAIN-WORSI <konainworsi@gmail.com>
2026-02-26 13:47:58 +0545

    first commit

---
+++ day_1.py
@@ -0,0 +1,35 @@
+
+ # number1 = int(input("Enter a number: "))
+ # number2 = int(input("Enter a number: "))
+
+ #add = number1 + number2;
+#sub = number1 - number2;
+#print("Addition: ", add)
+#print("Substraction: ",  sub)
+
+
+#number1 = int(input("Enter a number: "))
+#number2 = int(input("Enter a number: "))
+#number3 = int(input("Enter a number: "))
+#avg = (number1 + number2 + number3) / 3
+#print(avg)
+
+
+#days = int(input("Enter the number of days: "))
+
+#to_year = float((days/30)/12)
+#print(to_year)
+
+#x = int(input("Enter a number: "))
+#y = int(input("Enter a number: "))
+
+#print((x**3) + (3 * (x**2) * y) + (3 * x * (y**2)) + (y**3))
+
+l = int(input("Enter the length: "))
+b = int(input("Enter the breadth: "))
+
+if(l == b):
+    print("It is a Square")
+
+else:
+    print("It is a rectangle")
---
+++ day_2.py
@@ -0,0 +1,56 @@
+#a = int(input("Enter the first number(a): "))
+#b = int(input("Enter the second number(b): "))
+
+#print("Before Swapping;")
+#print("a: ", a)
+#print("b: ", b)
+
+#a, b = b, a
+#print("After Swapping;")
+#print("a: ", a)
+#print("b: ", b)
+
+#item1 = input("Enter the name of first product: ")
+#price1 = int(input("Enter the price of first product: "))
+
+#item2 = input("Enter the name of second product: ")
+#price2 = int(input("Enter thr price of second product: "))
+
+#item3 = input("Enter the name of third product: ")
+#price3 = int(input("Enter the price of third product: "))
+
+#total_cost = price1 + price2 + price3
+#tax_amount = total_cost * 0.13
+
+#total_amount = total_cost + tax_amount
+
+#print("--------Total Bill----------")
+#print("Item name: ",item1," Price: ", price1, end="")
+#print("\nItem name: ",item2, " Price: ", price2, end="")
+#print("\nItem name: ",item3, " Price: ", price3, end="")
+#print("\nTotal cost: ", total_cost, end="")
+#print("\nTax amount: ", tax_amount, end="")
+#print("\nTotal amount with tax amount is ", total_amount)
+
+
+#subject1 = input("Enter the name of the subject: ")
+#marks1 = int(input("Enter the marks: "))
+
+#subject2 = input("Enter the name of the subject: ")
+#marks2 = int(input("Enter the maerks of the subject: "))
+
+#subject3 = input("Enter the name of the subject: ")
+#marks3 = int(input("Enter the marks of the subject: "))
+
+#total_marks = marks1 + marks2 + marks3
+#avg = total_marks / 3
+
+#print("------Report Card------")
+#print("Subject: ", subject1," Marks: ", marks1, end="")
+#print("\nSubject: ", subject2, " Marks: ", marks2, end="")
+#print("\nSubject: ", subject3, " Marks: ", marks3, end="")
+#print("\nTotal Marks: ", total_marks, end="")
+#print("\nAverage: ", avg, end="")
+
+
+print(type(5.0))
