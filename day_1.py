2eb33f3edea8505ed12dfbdccd4bc9b87622ece6
KONAIN-WORSI <konainworsi@gmail.com>
2026-02-26 13:53:52 +0545

    first commit

--- day_1.py
+++ day_1.py
@@ -1,29 +1,29 @@
 
- # number1 = int(input("Enter a number: "))
- # number2 = int(input("Enter a number: "))
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
 
- #add = number1 + number2;
-#sub = number1 - number2;
-#print("Addition: ", add)
-#print("Substraction: ",  sub)
-
-
-#number1 = int(input("Enter a number: "))
-#number2 = int(input("Enter a number: "))
-#number3 = int(input("Enter a number: "))
-#avg = (number1 + number2 + number3) / 3
-#print(avg)
+add = number1 + number2
+sub = number1 - number2
+print("Addition: ", add)
+print("Substraction: ",  sub)
 
 
-#days = int(input("Enter the number of days: "))
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+number3 = int(input("Enter a number: "))
+avg = (number1 + number2 + number3) / 3
+print(avg)
 
-#to_year = float((days/30)/12)
-#print(to_year)
 
-#x = int(input("Enter a number: "))
-#y = int(input("Enter a number: "))
+days = int(input("Enter the number of days: "))
 
-#print((x**3) + (3 * (x**2) * y) + (3 * x * (y**2)) + (y**3))
+to_year = float((days/30)/12)
+print(to_year)
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+
+print((x**3) + (3 * (x**2) * y) + (3 * x * (y**2)) + (y**3))
 
 l = int(input("Enter the length: "))
 b = int(input("Enter the breadth: "))
