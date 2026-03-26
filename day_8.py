6a662b838cb7a03335357d8bedebe6cf239f2ac5
KONAIN-WORSI <konainworsi@gmail.com>
2026-03-26 17:24:27 +0545

    corrected some error in the existing code

--- day_8.py
+++ day_8.py
@@ -54,7 +54,7 @@
 
 
 a = [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
-b = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+b = [[2, 7, 6], [9, 5, 0], [4, 3, 8]]
 
 max_a = a[0][0]
 min_a = a[0][0]
@@ -73,20 +73,47 @@ for i in range(len(a)):
         else:
             add_below += a[i][j]
 
-        if i > max_a:
-            max_a = i
-        elif i < min_a:
-            min_a = i
+        if a[i][j] > max_a:
+            max_a = a[i][j]
+        elif a[i][j] < min_a:
+            min_a = a[i][j]
 
     
 print("Addition of diagonal number: ", add_d)
 print("Addition of above the diagonal number: ", add_above)
 print("Addition of below the diagonal number: ", add_below)
-print(max_a)
-print(min_a)
-    
-        
+print("Maximum number: ", max_a)
+print("Minimum number: ", min_a)
+print()
 
+
+max_b = b[0][0]
+min_b = b[0][0]
+
+add_d = 0
+add_above = 0
+add_below = 0
+
+for i in range(len(b)):
+    for j in range(len(b[i])):
+        if i == j:
+            add_d += b[i][j]
+        elif i < j:
+            add_above += b[i][j]
+        else:
+            add_below += b[i][j]
+
+        if b[i][j] > max_b:
+            max_b = b[i][j]
+        elif b[i][j] < min_b:
+            min_b = b[i][j]
+
+    
+print("Addition of diagonal number: ", add_d)
+print("Addition of above the diagonal number: ", add_above)
+print("Addition of below the diagonal number: ", add_below)
+print("Maximum number: ", max_b)
+print("Minimum number: ", min_b)
 
 
 
